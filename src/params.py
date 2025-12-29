f368b2afb799371ae44e764ed74a81233b0d67a6
medinajorge <medinahdezjorge@gmail.com>
2025-12-29 18:48:51 +0100

    preprocessing: v0

--- src/params.py
+++ src/params.py
@@ -4,3 +4,6 @@ from pathlib import Path
 _app_path = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.dirname(_app_path)
 DATA_PATH: Path = Path(os.path.join(ROOT, 'data'))
+BATHYMETRY_PATH: Path = Path(os.path.join(_app_path, 'data/BathymetryData.dat'))
+
+FEATURES = ['x', 'y', 'z', 'sin t', 'cos t', 'sin h', 'cos h', 'bathymetry', ' dt', 'v']
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from numba import njit
 import calendar
+from copy import deepcopy
+from pvlib import solarposition
 
 from phdu import pd_utils
 
@@ -87,7 +89,7 @@ def replace_dt_zeros(delta_t, by="mean", threshold=1e-8):
 
 def compute_dt(t, year, replace_zero_by="mean"):
     if t.size < 2:
-        return np.array([0])
+        return np.array([], dtype=np.float64)
     else:
         dt = (t[1:] - t[:-1])
         is_leap = get_leap_year(year)
@@ -179,8 +181,201 @@ def temporal_extension(X, Year, mode="all", **kwargs):
     ndays = np.array([time_ext(x, year) for x, year in zip(X, Year)])
     return ndays
 
+@njit
+def vectorized_is_leap(years):
+    """
+    Create a boolean mask where True indicates leap years
+    """
+    is_leap_mask = np.logical_or(np.logical_and(years % 4 == 0, years % 100 != 0), years % 400 == 0)
+    return is_leap_mask
+
+def _get_hour_angle(time_index, lat, lon):
+    """
+    Returns the hour angle in radians
+    """
+    solar_position = solarposition.get_solarposition(time_index, lat, lon, method='nrel_numpy')
+    hour_angle = solarposition.hour_angle(time_index, lon, solar_position.equation_of_time.values)
+    return hour_angle * np.pi / 180
+
+def hour_angle_from_trajectory(x, year, is_leap=None):
+    """
+    Returns the hour angle in radians
+    """
+    if is_leap is None:
+        is_leap = vectorized_is_leap(year)
+    lat, lon, day = x[:3]
+    day %= (365 + is_leap.astype(int))
+    T0 = pd.Timestamp(year=year[0], month=1, day=1, tz='UTC')
+    time_index = T0 + pd.to_timedelta(day, unit='D')
+    return _get_hour_angle(time_index, lat, lon)
+
+@njit
+def great_circle_distance(lat1, lon1, lat2, lon2):
+    """
+    Multiply by radius of Earth to get distance in km.
+    Assumes lat1, lon1, lat2, lon2 are all in radians.
+    """
+    # Differences in coordinates
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # Haversine formula
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    d = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    return d
+
+@njit
+def great_circle_distance_by_time_step(x):
+    """
+    Great circle distance between consecutive points in a trajectory.
+    x is a 2 x n array of latitudes and longitudes in radians.
+    """
+    n = x.shape[1]
+    d = np.empty(n-1)
+    for i in range(n-1):
+        d[i] = great_circle_distance(x[0, i], x[1, i], x[0, i+1], x[1, i+1])
+    return d
+
+def make_periodic(z, year, added_dt=False, to_origin=None, velocity='norm', replace_zero_by="mean", diff=False, diff_idxs=None, add_absolute_z=False, add_hour_angle=True):
+    """
+    Attributes:
+
+        - z:                     (lat, lon, t) vector with shape (3, length)
+
+        - year:                  array of year values. Used to compute the days per year and dt values.
+
+        - added_dt:              Bool. If true, returns the vector except for the last point, where dt is undefined. Mainly thought for the equal-time case.
+
+        - to_origin:             "time":             Shift initial time to 1 Jan.
+
+        - velocity:              None:               Does not add velocity.
+                                 "arch-segment":     Velocities in the SN-WE components.
+                                 "x-y-z":            Velocities as the derivatives w.r.t. time of x, y, z.
+
+        - replace_zero_by:       "closest":          When computing velocity, sometimes there are measurements done at the same time => dt = 0 y v=dx/dt leads to errors.
+                                                     Replaces zero with idx i by the mean of the closest non-zero dts (mean{dt[j], dt[k]} with dt[j], dt[k] non-zero and j,k closest idxs such that j>i, k<i).
+                                 "mean":             Replace zero with the mean dt between measurements in the trajectory.
+        - diff:                  Bool. If True, returns the difference in each magnitude (except for the velocity)
+
+        - diff_idxs:             List of indexes of the variables to be differentiated. If None, all variables are differentiated.
+
+    Returns:
+
+        periodic_x := (x, y, z, sin t, cos t, sin h, cos h, {weather vars},  {dt}, {velocity_vars})
+
+    Considerations:
+    {x, y, z} = {cos(theta) cos(phi), cos(theta) sin(phi), sin(theta)}
+    theta = lat (not the polar angle)
+    """
+    x = z.copy()
+    year_arr = np.array(year)
+    is_leap = vectorized_is_leap(year_arr)
+    if to_origin in ["time", "all"]:
+        x[2] = (x[2] - x[2,0])
+        x[2] = rescale_dt(x[2], is_leap)
+
+    t_angle = (2*np.pi) * x[2] # maybe / 366 for leap
+    t_angle[is_leap] /= 366
+    t_angle[~is_leap] /= 365
+    theta = (np.pi/180) * x[0]
+    phi = (np.pi/180) * x[1]
+    cos_theta = np.cos(theta)
+    if add_hour_angle:
+        periodic_x = np.empty((7, x.shape[1]))
+        hour_angle = hour_angle_from_trajectory(x, year_arr, is_leap=is_leap)
+        periodic_x[5] = np.sin(hour_angle)
+        periodic_x[6] = np.cos(hour_angle)
+    else:
+        periodic_x = np.empty((5, x.shape[1]))
+    periodic_x[0] = cos_theta * np.cos(phi)
+    periodic_x[1] = cos_theta * np.sin(phi)
+    periodic_x[2] = np.sin(theta)
+    periodic_x[3] = np.sin(t_angle)
+    periodic_x[4] = np.cos(t_angle)
+
+    if add_absolute_z:
+        periodic_x = np.vstack((periodic_x, np.abs(periodic_x[2])))
+
+    if x.shape[0] > 3: # there are weather variables or/and dt
+        periodic_x = np.vstack([periodic_x, x[3:]])
+
+    if velocity is not None:
+        delta_t = compute_dt(x[2], year, replace_zero_by=replace_zero_by)
+
+        if velocity == "x-y-z":
+            v = (periodic_x[:3, 1:] - periodic_x[:3, :-1]) / delta_t
+        elif velocity == "norm":
+            v = great_circle_distance_by_time_step((np.pi/180) * x[:2]) / delta_t
+            v = v[None]
+        else:
+            raise ValueError(f"velocity {velocity} not valid. Available: 'x-y-z', 'norm'.")
+
+        if diff_idxs is None:
+            periodic_x = np.vstack([np.diff(periodic_x, axis=1) if diff else periodic_x[:, :-1], # velocity undefined for the last point.
+                                    v])
+        elif len(diff_idxs) > 0:
+            no_diff_idxs = np.array([i for i in np.arange(periodic_x.shape[0]) if i not in diff_idxs])
+            periodic_x = np.vstack([periodic_x[no_diff_idxs, :-1],
+                                    np.diff(periodic_x[diff_idxs], axis=1) if diff else periodic_x[diff_idxs, :-1],
+                                    v])
+        else:
+            periodic_x = np.vstack([periodic_x[:, :-1], v])
+    else:
+        if added_dt:
+            if diff_idxs is None:
+                periodic_x = np.diff(periodic_x, axis=1)
+            elif len(diff_idxs) > 0:
+                no_diff_idxs = np.array([i for i in np.arange(periodic_x.shape[0]) if i not in diff_idxs])
+                periodic_x = np.vstack([periodic_x[no_diff_idxs, :-1],
+                                        np.diff(periodic_x[diff_idxs], axis=1) if diff else periodic_x[diff_idxs, :-1]
+                                       ])
+            else:
+                periodic_x = periodic_x[:,:-1]
+    return periodic_x, year[:periodic_x.shape[1]]
+
+def add_bathymetry_data(Z, X=None):
+    print("Adding bathymetry data")
+    if X is None:
+        X = deepcopy(Z)
+
+    bathymetry_data = np.genfromtxt(params.BATHYMETRY_PATH,
+                     skip_header=0,
+                     skip_footer=0,
+                     names=None,
+                     delimiter=' ')
+
+    ground = bathymetry_data > 0
+    bathymetry_data[ground] = 0
+
+    lon_edges = np.arange(-180, 180.25, 0.25)
+    lon_centers = 0.5 * (lon_edges[1:] + lon_edges[:-1])
+    lat_edges = np.arange(90, -90.25, -0.25)
+    lat_centers = 0.5 * (lat_edges[1:] + lat_edges[:-1])
+
+    @njit
+    def find_closest(lat, lon):
+        i = np.abs(lat - lat_centers).argmin()
+        j = np.abs(lon - lon_centers).argmin()
+        return bathymetry_data[i,j]
+
+    Z_new = []
+    for z, x in zip(Z, X):
+        bathymetry_x = np.array([find_closest(*x[:2, i]) for i in range(x.shape[1])])
+        bathymetry_x[(z == 0).all(axis=0)] = 0
+        Z_new.append(np.vstack([z, bathymetry_x]))
+    return Z_new
+
+def check_format(df):
+    required_cols = {'DATE_TIME', 'ID', 'LATITUDE', 'LONGITUDE'}
+    missing = required_cols - set(df.columns)
+    if len(missing) > 0:
+        raise ValueError(f"Missing columns: {missing}")
+    return
+
 def load_data(path=os.path.join(params.DATA_PATH, 'dataset.csv')):
     df = pd.read_csv(path)
+    check_format(df)
+
     df['DATE_TIME'] = pd.to_datetime(df['DATE_TIME'])
     dt = df.DATE_TIME.dt
     df['year'] = df.DATE_TIME.dt.year
@@ -188,18 +383,18 @@ def load_data(path=os.path.join(params.DATA_PATH, 'dataset.csv')):
 
     # get trajectories and year
     X = df.groupby("ID").apply(lambda x: x[['LATITUDE', 'LONGITUDE', 'day']].values.T)
-    year = df.groupby("ID").apply(lambda x: x.year.values)
-    year = year.loc[X.index]
+    Year = df.groupby("ID").apply(lambda x: x.year.values)
+    Year = Year.loc[X.index]
 
     min_dt = 0.25/24
-    X, year = undersample_trajectories(X, year, dt_threshold=min_dt)
+    X, Year = undersample_trajectories(X, Year, dt_threshold=min_dt)
 
     # metadata
     min_days = 5
     min_observations = 50
     num_observations = X.apply(lambda x: x.shape[1])
     num_observations.name = 'num_observations'
-    ndays = pd.Series(temporal_extension(X, year, mode='all'), index=X.index, name='num_days')
+    ndays = pd.Series(temporal_extension(X, Year, mode='all'), index=X.index, name='num_days')
     metadata = pd.concat([num_observations, ndays], axis=1)
 
     is_hq = (metadata.num_observations >= min_observations) & (metadata.num_days >= min_days)
@@ -209,4 +404,18 @@ def load_data(path=os.path.join(params.DATA_PATH, 'dataset.csv')):
     N = metadata.shape[0]
     print(f"Loaded {N} trajectories")
     print(f"Low tracking quality trajectories: {(~is_hq).values.sum()}/{N}")
-    return X, year, metadata
+    return X, Year, metadata
+
+def preprocess(path=os.path.join(params.DATA_PATH, 'dataset.csv')):
+    X, Year, metadata = load_data(path)
+
+    X = add_bathymetry_data(X)
+
+    print("Computing time delta")
+    X = [np.vstack([x, np.hstack([compute_dt(x[2], y), 0])]) for x, y in zip(X, Year)] # added 0 to be able to concatenate. Later will be removed
+
+    print("Handling discontinuities:\n\t-(lat, lon) -> (x, y, z)\n\t-day -> (sin, cos)\n\t-hour angle -> (sin, cos)")
+    make_periodic_kwargs = dict(added_dt=True, velocity='norm', to_origin=None, replace_zero_by="mean", diff=False, add_absolute_z=False)
+    X, Year = zip(*[make_periodic(x, year, **make_periodic_kwargs) for (x, year) in zip(X, Year)])
+
+    return X, Year, metadata
